254d6aeb3397914f24376e9af1303b006efb5be1
山形　彰仁 <yama@yamagataakihitonoMacBook-Pro.local>
2023-04-27 16:27:11 +0900

    devise導入完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions:      'users/sessions'   
+  # } 
+  
+  # devise_scope :user do
+  #   get "user/:id", :to => "users/registrations#detail"
+  #   get "signup",   :to => "users/registrations#new"
+  #   get "login",    :to => "users/sessions#new"
+  #   get "logout",   :to => "users/sessions#destroy"
+  # end
+
+  devise_for :users
+  resources  :users
   get 'static_pages/home'
 end
